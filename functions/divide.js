7465fbdbcf2fe66b6ca7828b44c33076a983216a
Craig Rodrigues <PagingCraig@gmail.com>
2018-04-03 17:44:13 -0400

    Now fix numerator

--- functions/divide.js
+++ functions/divide.js
@@ -1,6 +1,6 @@
 /* 
 Divide
 
-- Divides two values, `x / y` (x = numberator, y = denominator)
+- Divides two values, `x / y` (x = numerator, y = denominator)
 - Takes an array and a value and divides that value (as the denominator) to every item in the array and returns a **NEW array** (does not mutate)
 */
